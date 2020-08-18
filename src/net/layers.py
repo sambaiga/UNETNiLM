df21ff3f81652a68fc2b52aa992abdfd2eb6d15a
sambaiga <sambaiga@gmail.com>
2020-08-18 19:04:11 +0100

    update unet

--- src/net/layers.py
+++ src/net/layers.py
@@ -151,3 +151,37 @@ class Encoder(nn.Module):
         return feats
         
 
+class AttentionLayer(nn.Module):
+    #https://fleuret.org/git-extract/pytorch/attentiontoy1d.py
+    def __init__(self, in_channels, out_channels, key_channels):
+        super(AttentionLayer, self).__init__()
+        self.conv_Q = nn.Conv1d(in_channels, key_channels, kernel_size = 1, bias = False)
+        self.conv_K = nn.Conv1d(in_channels, key_channels, kernel_size = 1, bias = False)
+        self.conv_V = nn.Conv1d(in_channels, out_channels, kernel_size = 1, bias = False)
+        nn.utils.weight_norm(self.conv_Q)    
+        nn.init.xavier_uniform_(self.conv_Q.weight)
+        nn.utils.weight_norm(self.conv_K)    
+        nn.init.xavier_uniform_(self.conv_K.weight)
+        nn.utils.weight_norm(self.conv_V)    
+        nn.init.xavier_uniform_(self.conv_V.weight)
+
+    def forward(self, x):
+        Q = self.conv_Q(x)
+        K = self.conv_K(x)
+        V = self.conv_V(x)
+        A = Q.permute(0, 2, 1).matmul(K).softmax(2)
+        x = A.matmul(V.permute(0, 2, 1)).permute(0, 2, 1)
+        return x
+
+    def __repr__(self):
+        return self._get_name() + \
+            '(in_channels={}, out_channels={}, key_channels={})'.format(
+                self.conv_Q.in_channels,
+                self.conv_V.out_channels,
+                self.conv_K.out_channels
+            )
+
+    def attention(self, x):
+        Q = self.conv_Q(x)
+        K = self.conv_K(x)
+        return Q.permute(0, 2, 1).matmul(K).softmax(2)
